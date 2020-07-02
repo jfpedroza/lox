62f59b346cec75d6563fdcc4c22c9e0a20a7ad53
Jhon Pedroza <jhon@pedroza.me>
2020-07-01 22:05:07 -0500

    Add Lox struct and fix environments in the repl

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,76 @@ mod test_utils;
 mod utils;
 mod value;
 
+use error::print_err;
 use eval::Interpreter;
-use failure::Fallible;
+use failure::{Fallible, ResultExt};
 use lexer::Scanner;
 use parser::Parser;
+use rustyline::{config::Configurer, error::ReadlineError, Editor};
+use std::ffi::OsStr;
+use std::io::{stdin, Read};
+use std::path::Path;
 
-pub fn run(input: &str) -> Fallible<()> {
-    let mut scanner = Scanner::new(input);
-    let tokens = scanner.scan_tokens()?;
+pub struct Lox {
+    inter: Interpreter,
+}
 
-    let mut parser = Parser::new(&tokens);
-    let stmts = parser.parse()?;
+impl Lox {
+    pub fn new() -> Self {
+        Lox {
+            inter: Interpreter::new(),
+        }
+    }
 
-    let mut interpreter = Interpreter::new();
-    interpreter.interpret(stmts)?;
+    pub fn run(&mut self, input: &str) -> Fallible<()> {
+        let mut scanner = Scanner::new(input);
+        let tokens = scanner.scan_tokens()?;
 
-    Ok(())
+        let mut parser = Parser::new(&tokens);
+        let stmts = parser.parse()?;
+
+        self.inter.interpret(stmts)?;
+
+        Ok(())
+    }
+
+    pub fn run_file(&mut self, path: &OsStr) -> Fallible<()> {
+        let content = if path == "-" {
+            let mut content = String::new();
+            stdin()
+                .lock()
+                .read_to_string(&mut content)
+                .context("Could not read from stdin")?;
+            content
+        } else {
+            let path = Path::new(path);
+            let context = format!("Could not read '{}'", path.display());
+            std::fs::read_to_string(path).context(context)?
+        };
+
+        self.run(&content)
+    }
+
+    pub fn run_prompt(&mut self) -> Fallible<()> {
+        let mut rl = Editor::<()>::new();
+        rl.set_auto_add_history(true);
+
+        println!("Lox 0.0.1");
+        println!("Press Ctrl+D to exit\n");
+
+        loop {
+            match rl.readline("\x1b[1;34mlox\x1b[0m> ") {
+                Ok(line) if line.is_empty() => (),
+                Ok(line) => match self.run(&line) {
+                    Ok(()) => (),
+                    Err(err) => print_err(&err),
+                },
+                Err(ReadlineError::Interrupted) => (),
+                Err(ReadlineError::Eof) => break,
+                Err(err) => return Err(err.into()),
+            }
+        }
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,28 @@
 extern crate lox;
 
-use failure::{Fallible, ResultExt};
+use failure::Fallible;
 use lox::error::*;
 use lox::*;
-use rustyline::{config::Configurer, error::ReadlineError, Editor};
-use std::ffi::OsStr;
-use std::io::{stdin, Read};
-use std::path::Path;
 
-fn main() {
+fn program() -> Fallible<()> {
     let args = std::env::args_os().skip(1).collect::<Vec<_>>();
-    let res = match args.len() {
-        0 => run_prompt(),
-        1 => run_file(&args[0]),
+    let mut lox = Lox::new();
+    match args.len() {
+        0 => lox.run_prompt(),
+        1 => lox.run_file(&args[0]),
         _ => {
             eprintln!("Usage: lox [script]");
             std::process::exit(64);
         }
-    };
+    }
+}
 
-    match res {
+fn main() {
+    match program() {
         Ok(()) => (),
         Err(ref err) => {
             print_err(err);
             std::process::exit(exit_code(err));
         }
     }
-}
-
-fn run_file(path: &OsStr) -> Fallible<()> {
-    let content = if path == "-" {
-        let mut content = String::new();
-        stdin()
-            .lock()
-            .read_to_string(&mut content)
-            .context("Could not read from stdin")?;
-        content
-    } else {
-        let path = Path::new(path);
-        let context = format!("Could not read '{}'", path.display());
-        std::fs::read_to_string(path).context(context)?
-    };
-
-    run(&content)
-}
-
-fn run_prompt() -> Fallible<()> {
-    let mut rl = Editor::<()>::new();
-    rl.set_auto_add_history(true);
-
-    println!("Lox 0.0.1");
-    println!("Press Ctrl+D to exit\n");
-
-    loop {
-        match rl.readline("\x1b[1;34mlox\x1b[0m> ") {
-            Ok(line) if line.is_empty() => (),
-            Ok(line) => match run(&line) {
-                Ok(()) => (),
-                Err(err) => print_err(&err),
-            },
-            Err(ReadlineError::Interrupted) => (),
-            Err(ReadlineError::Eof) => break,
-            Err(err) => return Err(err.into()),
-        }
-    }
-
-    Ok(())
 }
